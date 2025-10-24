bdd854199e543fbca656220bbc6b285c768a31b6
SoulThief1322 <alenko2007@gmail.com>
2025-10-24 09:49:06 +0300

    Animal class refactored

---
+++ Data/Models/Animal.cs
@@ -0,0 +1,24 @@
+namespace CodeRefactoring.Models
+{
+    public class Animal
+    {
+        public int Id;
+        public string Names = string.Empty;
+        public string Owner = string.Empty;
+        public int Age;
+        public string Type = string.Empty;
+        public bool IsSick = false;
+        public string Notes = "";
+
+        public void MakeOlder()
+        {
+            Age += 3;
+        }
+
+        public void Heal()
+        {
+            IsSick = false;
+            Notes = "feeling ok i guess";
+        }
+    }
+}
